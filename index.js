9a951816d2fcc7b64a30388a087bd5df730780d2
ebanchik <ebanchik@gmail.com>
2023-12-14 14:06:48 -0500

    adding webpack

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+var moment = require('moment');
+
+var datetime = moment().startOf("day").fromNow();
+var birthTime = moment("20231225").fromNow();
+
+console.log(datetime);
+console.log(birthTime + " Days it will be Christmas");
